143b21e65bb763bc2d313ba85c26b4fc7237e966
Ruan Pasta <ruan_augusto_pasta@hotmail.com>
2026-02-08 23:39:27 -0300

    feat: limit the activities list to 20 by default

--- project/resources/sql/activities/list.sql
+++ project/resources/sql/activities/list.sql
@@ -1,4 +1,5 @@
 SELECT * FROM api.activities
 WHERE (?::date IS NULL OR created_at = ?::date)
 			AND (?::text IS NULL OR activity = ?::text)
-			AND (?::text IS NULL OR activity_type = ?::text);
+			AND (?::text IS NULL OR activity_type = ?::text)
+LIMIT 20;
